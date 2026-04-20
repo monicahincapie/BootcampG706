74d75dc1d5309825f78d20a8b152c6ccb53af04e
git monica <2024hincapienaranjomonicq@gmail.com>
2026-04-20 13:03:15 -0500

    20 de abril

---
+++ mision2/chatv1/chatv1.py
@@ -0,0 +1,7 @@
+from sklearn.feature_extraction.text import CountVectorizer
+# CountVectorizer convierte texto en un vector
+from sklearn.naive_bayes import MultinomialNB
+"""
+MultinomialNB modelo de inteligencia
+ artificial que aprende relaciones entre texto y respuestas
+"""
---
+++ mision2/chatv1/implementacion.md
@@ -0,0 +1,11 @@
+# Guia de implementacion del chatbot (supervisado)
+
+import nltk
+nltk.download('punkt')
+python -c "import nltk.download('punkt')
+
+visual code
+python -m pip install nltk
+
+instalar librerias
+"pip install scikit-learn nltk numpy gym==0.26.2 gym-notices"
